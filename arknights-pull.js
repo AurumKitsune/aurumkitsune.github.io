4fe30f7a5d3fe7ed2333626c31cc9a5d79bd3261
Aurum <AurumKitsune@gmail.com>
2022-12-22 13:19:00 -0700

    add yellow certs tick box

--- arknights-pull.js
+++ arknights-pull.js
@@ -2,10 +2,12 @@ const pullsElement = document.querySelector(".pulls");
 const orundumElement = document.querySelector("#orundum");
 const originiteElement = document.querySelector("#originite");
 const headhuntingElement = document.querySelector("#headhunting");
+const yellowCertsElement = document.querySelector("#yellow-certs");
 
 orundumElement.addEventListener("input", updatePulls);
 originiteElement.addEventListener("input", updatePulls);
 headhuntingElement.addEventListener("input", updatePulls);
+yellowCertsElement.addEventListener("input", updatePulls);
 
 updatePulls();
 
@@ -13,14 +15,12 @@ function updatePulls() {
 	let orundum = getIntFromElement(orundumElement);
 	let originite = getIntFromElement(originiteElement);
 	let headhunting = getIntFromElement(headhuntingElement);
-	// console.log("orundum: " + orundum + "  originite: " + originite + "  headhunting: " + headhunting);
 
-	// let yellowCerts = prompt("Do you have 258 yellow certs? (Yes/No)");
-	// if (yellowCerts.toLowerCase() === "yes") yellowCerts = 1;
-	// else yellowCerts = 0;
+	let yellowCerts = yellowCertsElement.checked ? 1 : 0;
 
-	// let total = Math.floor((((180*originite) + orundum) / 600) + headhunting + (38*yellowCerts));
-	let total = Math.floor((((180*originite) + orundum) / 600) + headhunting);
+	// console.log("orundum: " + orundum + "  originite: " + originite + "  headhunting: " + headhunting + "  certs: " + yellowCerts);
+	
+	let total = Math.floor((((180 * originite) + orundum) / 600) + headhunting + (38 * yellowCerts));
 
 	pullsElement.textContent = "You have " + total + " pulls";
 }
